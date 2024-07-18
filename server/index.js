ea88b38e2503df4987c741dd575b0f049e1b6df8
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-07-18 17:42:01 +0500

    added  file

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import authRoutes from './routes/auth.js'
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
+
 
 // configuration 
 
@@ -44,6 +46,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // ROUTES
 app.use('/auth', authRoutes);
+app.use("/users", userRoutes)
 
 // Mongoose Setup
 const PORT = process.env.PORT || 5000;
